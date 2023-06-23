07d7df0c0a5eda78dfb89db7a9caed049b57698e
Jeffrey Hsu <xfj12@outlook.com>
2023-06-23 09:45:26 +0800

    Bug fixed #2

--- MainUi/src/main.cpp
+++ MainUi/src/main.cpp
@@ -8,6 +8,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    a.connect(&a, SIGNAL(lastWindowClosed()), &a, SLOT(quit()));
     w.show();
     return a.exec();
 }
